053b27e2cb812833094d1617aac9a713011e517e
a_nuo <gaoluping1985@163.com>
2015-10-09 02:15:47 +0800

    eight class

--- js/app.js
+++ js/app.js
@@ -9,4 +9,9 @@ function makeCartScrollNicely() {
 window.onload = function() {
 	makeCartScrollNicely();
 	console.log("page loaded");
+
+	var $toggle = document.querySelector(".site__right-sidebar-toggle");
+	$toggle.addEventListener("click",function() {
+	  document.body.classList.toggle("js-show-right-sidebar");
+	});
 }
